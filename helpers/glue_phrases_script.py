888502c017932152d65fe3c0667e1eec3fd8af49
Alexey Bukhtiyarov <a.bukhtiyarov@yandex.ru>
2020-02-13 14:59:41 +0300

    glue phrases script

---
+++ helpers/glue_phrases_script.py
@@ -0,0 +1,15 @@
+import sys
+
+with open(sys.argv[1], 'r', encoding='utf-8') as f:
+    data = f.readlines()
+
+with open(sys.argv[1], 'w', encoding='utf-8') as f:
+    for el in data:
+        if '|' not in el:
+            f.write(el)
+            continue
+
+        el = el.replace(' ##', '')
+        if '##' not in el:
+            f.write(el)
+
