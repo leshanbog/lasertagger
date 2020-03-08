d431d87139d71ff4421092aa62bcd2d9411acf2f
students <alolbuhtijarov@almaren.yandex.ru>
2020-03-08 15:23:33 -0400

    run script

--- helpers/glue_phrases_script.py
+++ helpers/glue_phrases_script.py
@@ -3,7 +3,7 @@ import sys
 with open(sys.argv[1], 'r', encoding='utf-8') as f:
     data = f.readlines()
 
-with open(sys.argv[1], 'w', encoding='utf-8') as f:
+with open(sys.argv[2], 'w', encoding='utf-8') as f:
     for el in data:
         if '|' not in el:
             f.write(el)
